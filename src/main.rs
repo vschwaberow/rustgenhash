8f87f0e97a52c786b3f27070ce8a662f6c57c33e
Volker Schwaberow <volker@schwaberow.de>
2020-10-27 07:17:23 +0100

    Fixes #3 and implements Rust Crypto crates.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use structopt::StructOpt;
-use crypto::digest::Digest;
-use crypto::sha1::Sha1;
-use crypto::sha2::{Sha224, Sha256, Sha384, Sha512};
-use crypto::sha3::Sha3;
-use crypto::whirlpool::Whirlpool;
 use std::process::exit;
-use crypto::md5::Md5;
-use crypto::ripemd160::Ripemd160;
-
+use digest::Digest;
+use digest::generic_array::ArrayLength;
+use md5::Md5;
+use sha1::Sha1;
+use sha2::{Sha224, Sha256, Sha384, Sha512};
+use sha3::{Sha3_224, Sha3_256, Sha3_384, Sha3_512};
+use std::ops::Add;
+use ripemd160::Ripemd160;
+use whirlpool::Whirlpool;
 
 #[derive(Debug, StructOpt)]
 struct GenCmd {
@@ -29,13 +30,11 @@ fn match_valid() {
     exit(0);
 }
 
-fn create_hash<T:Digest>(password: String, mut hasher:T, algo: String) {
-    hasher.input_str(&password);
-    print_hash(&algo, hasher.result_str());
-}
-
-fn print_hash(algo:&str, password: String) {
-    println!("{} hash: {}", algo, password);
+fn create_hash<D>(password: String, mut hasher:D, algo: String) where D: Digest, D::OutputSize: Add,
+                                                                      <D::OutputSize as Add>::Output: ArrayLength<u8>
+{
+    hasher.update(&password.as_bytes());
+    println!("{} hash is: {:x}", algo, hasher.finalize());
 }
 
 fn main() {
@@ -52,10 +51,10 @@ fn main() {
             "sha256" => create_hash(args.password, Sha256::new(), "sha256".to_string()),
             "sha384" => create_hash(args.password, Sha384::new(), "sha384".to_string()),
             "sha512" => create_hash(args.password, Sha512::new(), "sha512".to_string()),
-            "sha3-224" => create_hash(args.password, Sha3::sha3_224(), "sha3-224".to_string()),
-            "sha3-256" => create_hash(args.password, Sha3::sha3_256(), "sha3-256".to_string()),
-            "sha3-384" => create_hash(args.password, Sha3::sha3_384(), "sha3-384".to_string()),
-            "sha3-512" => create_hash(args.password, Sha3::sha3_512(), "sha3-512".to_string()),
+            "sha3-224" => create_hash(args.password, Sha3_224::new(), "sha3-224".to_string()),
+            "sha3-256" => create_hash(args.password, Sha3_256::new(), "sha3-256".to_string()),
+            "sha3-384" => create_hash(args.password, Sha3_384::new(), "sha3-384".to_string()),
+            "sha3-512" => create_hash(args.password, Sha3_512::new(), "sha3-512".to_string()),
             "whirlpool" => create_hash(args.password, Whirlpool::new(), "whirlpool".to_string()),
             "ripemd160" => create_hash(args.password, Ripemd160::new(), "ripemd160".to_string()),
             _ => match_valid(),
