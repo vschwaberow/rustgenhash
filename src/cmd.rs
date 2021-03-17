bc7127ad2f3026db99dea26419f16461f4b3a875
Volker Schwaberow <volker@schwaberow.de>
2021-03-17 07:50:40 +0100

    Put structopt struct to single file

---
+++ src/cmd.rs
@@ -0,0 +1,45 @@
+
+use structopt::StructOpt;
+
+const ALGO_LONG_HELP: &str = r"A switch to provide the hash algorithm with which the provided
+string will be hashed. Supported are: blake2s, blake2b, gost94,
+groestl, md2, md4, md5, ripemd160, ripemd320, sha1, sha224, sha256,
+sha384, sha512, sha3-224, sha3-256, sha3-384, sha3-512, shabal192,
+shabal224, shabal256, shabal384, shabal512, streebog256,
+streebog512, whirlpool";
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    name = "rustgenhash",
+    about = "CLI utility to generate hashes for files and strings."
+)]
+pub enum Cmd {
+    File {
+        #[structopt(
+            short,
+            required = true,
+            long_help = ALGO_LONG_HELP,
+        )]
+        algorithm: String,
+        #[structopt(name = "FILENAME", required = true)]
+        input: String,
+    },
+    String {
+        #[structopt(
+            short,
+            required = true,
+            long_help = ALGO_LONG_HELP,
+        )]
+        algorithm: String,
+        #[structopt(name = "PASSWORD", required = true)]
+        password: String,
+    },
+    Stdio {
+        #[structopt(
+        short,
+        required = true,
+        long_help = ALGO_LONG_HELP,
+        )]
+        algorithm: String,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+mod cmd;
+use crate::cmd::Cmd;
+use std::{fs, io};
+use std::io::BufRead;
+use std::ops::Add;
+use std::process::exit;
+
 use blake2::{Blake2b, Blake2s};
-use digest::generic_array::ArrayLength;
 use digest::Digest;
+use digest::generic_array::ArrayLength;
 use gost94::*;
 use groestl::*;
 use md2::Md2;
@@ -12,57 +19,11 @@ use sha1::Sha1;
 use sha2::{Sha224, Sha256, Sha384, Sha512};
 use sha3::{Sha3_224, Sha3_256, Sha3_384, Sha3_512};
 use shabal::{Shabal192, Shabal224, Shabal256, Shabal384, Shabal512};
-use std::ops::Add;
-use std::process::exit;
-use std::{fs, io};
 use streebog::*;
 use structopt::StructOpt;
 use whirlpool::Whirlpool;
-use std::io::BufRead;
-
-const ALGO_LONG_HELP: &str = r"A switch to provide the hash algorithm with which the provided
-string will be hashed. Supported are: blake2s, blake2b, gost94,
-groestl, md2, md4, md5, ripemd160, ripemd320, sha1, sha224, sha256,
-sha384, sha512, sha3-224, sha3-256, sha3-384, sha3-512, shabal192,
-shabal224, shabal256, shabal384, shabal512, streebog256,
-streebog512, whirlpool";
 
 
-#[derive(StructOpt, Debug)]
-#[structopt(
-    name = "rustgenhash",
-    about = "CLI utility to generate hashes for files and strings."
-)]
-enum Cmd {
-    File {
-        #[structopt(
-            short,
-            required = true,
-            long_help = ALGO_LONG_HELP,
-        )]
-        algorithm: String,
-        #[structopt(name = "FILENAME", required = true)]
-        input: String,
-    },
-    String {
-        #[structopt(
-            short,
-            required = true,
-            long_help = ALGO_LONG_HELP,
-        )]
-        algorithm: String,
-        #[structopt(name = "PASSWORD", required = true)]
-        password: String,
-    },
-    Stdio {
-        #[structopt(
-        short,
-        required = true,
-        long_help = ALGO_LONG_HELP,
-        )]
-        algorithm: String,
-    },
-}
 
 fn match_invalid() {
     println!("You need to select a valid algorithm.");
