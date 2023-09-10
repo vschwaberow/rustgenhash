3d324f0884db97eef9f631a6834797a68465cf37
Volker Schwaberow <volker@schwaberow.de>
2023-09-10 07:42:23 +0200

    Development (#74)
    * Killed loop and substituted it by using mapping
    
    * Reduced memory allocs for properties
    
    * Updated cargo.toml and crates

--- src/hhhash.rs
+++ src/hhhash.rs
@@ -14,28 +14,20 @@ pub fn generate_hhhash(
 ) -> Result<String, Box<dyn Error>> {
 	let client =
 		Client::builder().timeout(Duration::from_secs(10)).build()?;
-	let resp = client.get(url).send();
-	match resp {
-		Ok(resp) => {
-			let headers = resp.headers();
+	let resp = client.get(url).send()?;
 
-			let mut header_names = Vec::new();
-			for header_name in headers.keys() {
-				header_names.push(header_name);
-			}
+	let header_names: Vec<_> = resp
+		.headers()
+		.keys()
+		.map(|header| header.as_str())
+		.collect();
+	let header_string = header_names.join("\n");
 
-			let header_string = header_names
-				.iter()
-				.map(|h| h.as_str())
-				.collect::<Vec<_>>()
-				.join("\n");
+	let mut hasher = sha2::Sha256::new();
+	hasher.update(header_string.as_bytes());
+	let hash = hasher.finalize();
+	let hash = format!("hhh:1:{}", hex::encode(hash));
 
-			let mut hasher = sha2::Sha256::new();
-			hasher.update(header_string.as_bytes());
-			let hash = hasher.finalize();
-			let hash = format!("hhh:1:{}", hex::encode(hash));
-			Ok(hash)
-		}
-		Err(e) => Err(Box::new(e)),
-	}
+	Ok(hash)
+
 }
