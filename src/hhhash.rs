aa3335fe87b95bcc43936053488a72f065abf500
Volker Schwaberow <volker@schwaberow.de>
2023-11-10 05:22:32 +0100

    Fixes #76 and adds proper handling for URLs

--- src/hhhash.rs
+++ src/hhhash.rs
@@ -8,13 +8,35 @@ use reqwest::blocking::Client;
 use sha2::Digest;
 use std::error::Error;
 use std::time::Duration;
+use url::Url;
 
+fn parse_url(url: &str) -> Result<Url, Box<dyn std::error::Error>> {
+	let parsed_url = match Url::parse(url) {
+		Ok(url) => match url.scheme() {
+			"http" | "https" => url,
+			_ => {
+				eprintln!("Error: URL {} has an invalid scheme. Only http or https are allowed.", url);
+				std::process::exit(1);
+			}
+		},
+		Err(_) => {
+			eprintln!("Warning: URL {} is not valid / complete. Assuming https.", url);
+			Url::parse(&format!("https://{}", url)).map_err(|e| {
+				Box::new(e) as Box<dyn std::error::Error>
+			})?
+		}
+	};
+	Ok(parsed_url)
+}
 pub fn generate_hhhash(
 	url: String,
 ) -> Result<String, Box<dyn Error>> {
+	let parsed_url = parse_url(&url)?;
+
 	let client =
 		Client::builder().timeout(Duration::from_secs(10)).build()?;
-	let resp = client.get(url).send()?;
+
+	let resp = client.get(parsed_url).send()?;
 
 	let header_names: Vec<_> = resp
 		.headers()
@@ -29,5 +51,4 @@ pub fn generate_hhhash(
 	let hash = format!("hhh:1:{}", hex::encode(hash));
 
 	Ok(hash)
-
 }
