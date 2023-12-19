4c7d0acc239b9e5e77df43d66f99c7f88236fa29
Volker Schwaberow <volker@schwaberow.de>
2023-12-19 04:56:24 +0100

    Refactor all modules to a library

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// Project: rustgenhash
+// File: analyze.rs
+// Author: Volker Schwaberow <volker@schwaberow.de>
+// Copyright (c) 2022 Volker Schwaberow
+
+pub mod lib {
+	pub mod analyze;
+	pub mod app;
+	pub mod hash;
+	pub mod hhhash;
+}
